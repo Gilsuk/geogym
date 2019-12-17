dde61ed1623cf1cb27d03c5ed2bd14942bbc2508
이주용 <wndyd121@gmail.com>
2019-12-17 19:20:39 +0900

    ggg

--- src/main/java/com/geogym/matching/service/MatchingService.java
+++ src/main/java/com/geogym/matching/service/MatchingService.java
@@ -1,7 +1,13 @@
 package com.geogym.matching.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import com.geogym.matching.dto.MatchingSchedule;
+import com.geogym.matching.exception.LessThanOneHour;
 import com.geogym.matching.exception.MatchingNotAvailable;
+import com.geogym.trainer.dto.Trainer;
+import com.geogym.user.dto.User;
 
 public interface MatchingService {
 	
@@ -17,9 +23,29 @@ public interface MatchingService {
 	 * 회원과 트레이너 일정에서 각각 삭제
 	 * 알림 발송
 	 * 환불
+	 * 
+	 * PT까지 남은 시간이 1시간 미만이면 예외 처리
+	 * 
 	 * @param schedule 취소할 일정
 	 * @param msg 취소 사유
 	 */
-	void cancle(MatchingSchedule schedule, String msg);
-
+	void cancle(MatchingSchedule schedule, String msg) throws LessThanOneHour;
+	
+	/**
+	 * PT신청시 정기권이 있는 트레이너인지 체크
+	 * 
+	 * @param user
+	 * @param trainer
+	 * @return
+	 */
+	boolean isSubscribedTrainer(User user, Trainer trainer);
+	
+	/**
+	 * 해당 유저의 PT 일정들 받아오기
+	 * 
+	 * @param user
+	 * @return
+	 */
+	List<MatchingSchedule> getPTInfo(User user, LocalDateTime today);
+	
 }
--- src/main/java/com/geogym/payment/service/PaymentService.java
+++ src/main/java/com/geogym/payment/service/PaymentService.java
@@ -1,5 +1,25 @@
 package com.geogym.payment.service;
 
-public interface PaymentService {
+import com.geogym.payment.exception.CoinNotEnoughException;
+import com.geogym.trainer.dto.Trainer;
+import com.geogym.user.dto.User;
 
+public interface PaymentService {
+	
+	/**
+	 * 근을 소비할때 호출
+	 * 유저의 근이 부족할때 예외 처리 해야함
+	 * 
+	 * @param amount - 소비될 코인의 양
+	 * @param user - 코인을 소비할 유저 객체
+	 */
+	void payByCoin(int amount, User user) throws CoinNotEnoughException;
+
+	/**
+	 * 정기권을 이용한 PT 신청시 횟수 차감
+	 * 
+	 * @param user
+	 * @param trainer
+	 */
+	void payBySeasonTicket(User user, Trainer trainer);
 }
