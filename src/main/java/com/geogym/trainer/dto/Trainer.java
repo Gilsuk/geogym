0b0700f1f2c4b8139b29d631887e761174da7f04
이주용 <wndyd121@gmail.com>
2019-12-19 20:22:57 +0900

    트레이너 DTO 수정

--- src/main/java/com/geogym/trainer/dto/Trainer.java
+++ src/main/java/com/geogym/trainer/dto/Trainer.java
@@ -3,26 +3,13 @@ package com.geogym.trainer.dto;
 // 이 부분은 일단 유저 DTO 가 나온 후에 트레이너 DTO에만 필요한 부분을 집어넣어야 할 것 같음
 public class Trainer {
 
-	private String name;
-	private int no;
-	public int tranerno;	// 트레이너 번호
-	public int tranerrank;	// 트레이너 근 수율 단위
-	public int tranerlevel;	// 트레이너 등급
-	@Override
-	public String toString() {
-		return "Trainer [name=" + name + ", no=" + no + "]";
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getNo() {
-		return no;
-	}
-	public void setNo(int no) {
-		this.no = no;
-	}
+	private int trainer_no;	// 트레이너 번호
+	private int user_no;
+	private String trainer_address;
+	private int trainer_price;	// 트레이너 근 수율 단위
+	private String trainer_profile;
+	private String trainer_hiredate;
+	private String trainer_resigndate;
+	private String trainer_name;	
 
 }
