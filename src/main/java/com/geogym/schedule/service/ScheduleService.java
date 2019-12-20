7dc6d0d64c00a4e9746a619cb56e9f3ea078ebdd
이주용 <wndyd121@gmail.com>
2019-12-20 19:05:45 +0900

    AvailableTime 클래스 이름 ScheduleService 이름 변경 / 그외 클래스 메소드 추가

---
+++ src/main/java/com/geogym/schedule/service/ScheduleService.java
@@ -0,0 +1,45 @@
+package com.geogym.schedule.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import com.geogym.matching.dto.MatchingSchedule;
+import com.geogym.trainer.dto.Trainer;
+import com.geogym.user.dto.User;
+
+public interface ScheduleService {
+	
+	/**
+	 * 트레이너의 빈 시간을 30분 단위로 끊어서 리스트로 리턴
+	 * 
+	 * 마지막 시간은 최소 1시간 텀은 필요하다
+	 * ex)트레이너의 2시에 스케줄이 있으면 1:30은 제외
+	 * 
+	 * @param trainer
+	 * @param locaDate
+	 * @return
+	 */
+	List<LocalTime> getAvilableTime(Trainer trainer, LocalDate locaDate);
+
+	/**
+	 * 
+	 * 
+	 * @param trainer
+	 * @param user
+	 * @param locaDatetime
+	 */
+	void setPTShcedule(Trainer trainer, User user, LocalDateTime locaDatetime);
+	
+	/**
+	 * 
+	 * 
+	 * @param trainer
+	 * @param user
+	 * @param locaDatetime
+	 */
+	void cancelPTSchedule(Trainer trainer, User user, LocalDateTime locaDatetime);
+	
+	List<MatchingSchedule> getPTSchedule(Trainer trainer, User user, LocalDateTime locaDatetime);
+}
